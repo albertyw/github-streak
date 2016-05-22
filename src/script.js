84b30e3d7d2d3c62707b4817dbfad5d8199a15e7
Albert Wang <albertyw@mit.edu>
2016-05-21 23:54:25 -0700

    Cleanup

--- src/script.js
+++ src/script.js
@@ -1,6 +1,3 @@
-console.log($);
-console.log('asdf');
-
 var html = '<div class="contrib-column contrib-column-first table-column">' +
 '  <span class="text-muted">Year of contributions</span>' +
 '  <span class="contrib-number">3091 total</span>' +
@@ -23,7 +20,3 @@ var html = '<div class="contrib-column contrib-column-first table-column">' +
 '</div>';
 
 $("#contributions-calendar").append(html);
-var lastYear = '<span id="last-year" class="boxed-group-inner"></span>';
-var longestStreak = '<span id="longest-streak" class="boxed-group-inner"></span>';
-var currentStreak = '<span id="current-streak" class="boxed-group-inner"></span>';
-$("#contributions-streak");
